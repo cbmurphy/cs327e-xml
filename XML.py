1ce08c6333fd780ec20f1f2644bfd9833e4acfa2
Collin Murphy <cbmurphy@utexas.edu>
2013-02-08 22:08:46 -0600

    Enhanced code and added comments

--- XML.py
+++ XML.py
@@ -1,46 +1,76 @@
 #!/usr/bin/env python
 
+# -------
+# imports
+# -------
+
 import xml.etree.ElementTree as ET
+import sys
 
-taglst = []    #list of tags
-nodes = []     #list of nodes corresponding to tag list
-
-inputlst = []  
-
-def parseTree(node):
-  taglst.append(node.tag)
-  nodes.append(node)
-  for x in node:
-    parseTree(x)
-
-def parseInput(node):
-  inputlst.append(node.tag)
-  for x in node:
-    parseInput(x)
-
-def traverseTree(node1, idx):
-  for child in node1:
-    if child.tag == inputlst[idx]:
-      #print "Found child: " + str(child.tag)
-      if idx == len(inputlst) - 1:
-        return True
-      return traverseTree(child, idx + 1)
-  return False
+taglst = []    #list of tags from XML
+nodes = []     #list of nodes corresponding to tag list in input
 
 # -------------
 # xml_print
 # -------------
 
 def xml_print(w, results):
+  
   """ 
   print the results
   w is a writer
   results is a list containing matches
   """
+  
   w.write(str(len(results)) + '\n')
   for num in results:
     w.write(str(num) + '\n')
 
+def parseTree(node):
+
+  ''' create list of tags in XML'''
+  
+  taglst.append(node.tag)
+  nodes.append(node)
+  for x in node:
+    parseTree(x)
+      
+
+def dfs_new(root, root2):
+
+  '''
+  depth first search function to check tree
+  root is the occurence of the first node
+  root2 is the input tree
+  '''
+  
+  len1 = len(root)
+  len2 = len(root2)
+
+  #check if input is empty or XML is smaller than input
+  if len2 == 0:
+    return True
+  if len1 == 0 and len2 != 0:
+    return False
+
+  #loop over tree to check for input
+  i = 0
+  j = 0
+  while ( i < len1 and j < len2):
+    if root[i].tag != root2[j].tag:
+      i += 1
+    else:
+      if dfs_new(root[i], root2[j]):
+        i += 1
+        j += 1
+        continue
+      else:
+        return False
+
+  if i == len1 and j != len2:
+    return False
+
+  return True
 
 # -------------
 # xml_solve
@@ -56,16 +86,24 @@ def xml_solve(r, w):
   xmlstr = r.read()
   root = ET.fromstring('<xml>' + xmlstr + '</xml>')
 
-  for idx, child  in enumerate(root):
-    if idx == 0:
-      parseTree(child)
-    else:
-      parseInput(child)
-
+  parseTree(root[0])
   results = []
   for idx, tag in enumerate(taglst):
-    if tag == inputlst[0]:
-      if traverseTree(nodes[idx], 1):
+    if tag == root[1].tag:
+      if dfs_new(nodes[idx], root[1]):
         results.append(idx+1)
 
   xml_print(w, results)
+
+# ----
+# main
+# ----
+
+if __name__ == "__main__":
+  try:
+    f = open("2.xml")
+    xml_solve(f, sys.stdout)
+  except:
+    print "File missing!"
+else:
+  xml_solve(sys.stdin, sys.stdout)
