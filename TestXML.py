3671b17c0aa885a5dae3e0105c348bea0e59c295
Jianyong Meng <mengjy02@gmail.com>
2013-02-12 13:02:13 -0600

    added empty functions for the unit test code

---
+++ TestXML.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python
+
+"""
+To test the program:
+    % TestXML.py >& TestXML.py.out
+"""
+
+# -------
+# imports
+# -------
+
+import StringIO
+import unittest
+
+from XML import * 
+
+# -----------
+# TestXML
+# -----------
+
+class TestXML (unittest.TestCase) :
+    # -------
+    # print 
+    # -------
+
+    def test_print_1(self):
+        self.assert_(0)
+
+    def test_print_2(self):
+        self.assert_(0)
+
+    def test_print_3(self):
+        self.assert_(0)
+
+    # -----------
+    # parseTree 
+    # -----------
+
+    def test_parseTree_1(self):
+        self.assert_(0)
+
+    def test_parseTree_2(self):
+        self.assert_(0)
+
+    def test_parseTree_3(self):
+        self.assert_(0)
+
+    # --------
+    # dfs_new 
+    # --------
+
+    def test_dfs_new_1(self):
+        self.assert_(0)
+
+    def test_dfs_new_2(self):
+        self.assert_(0)
+
+    def test_dfs_new_2(self):
+        self.assert_(0)
+
+    # -----
+    # solve
+    # -----
+
+    def test_solve_1(self):
+        self.assert_(0)
+
+    def test_solve_2(self):
+        self.assert_(0)
+
+    def test_solve_3(self):
+        self.assert_(0)
+# ----
+# main
+# ----
+
+print "TestXML.py"
+unittest.main()
+print "Done."
