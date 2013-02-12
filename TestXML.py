8f3f4aa936c5163b69d66b2262aa131b47402ff6
Jianyong Meng <mengjy02@gmail.com>
2013-02-12 13:39:53 -0600

    added simple unit tests for xml_print

--- TestXML.py
+++ TestXML.py
@@ -19,57 +19,66 @@ from XML import *
 # -----------
 
 class TestXML (unittest.TestCase) :
-    # -------
-    # print 
-    # -------
+  # -------
+  # print 
+  # -------
 
-    def test_print_1(self):
-        self.assert_(0)
+  def test_print_1(self):
+    w = StringIO.StringIO()
+    res = [1]
+    xml_print(w, res)
+    self.assert_(w.getvalue() == "1\n1\n")
 
-    def test_print_2(self):
-        self.assert_(0)
+  def test_print_2(self):
+    w = StringIO.StringIO()
+    res = []
+    xml_print(w, res)
+    self.assert_(w.getvalue() == "0\n")
 
-    def test_print_3(self):
-        self.assert_(0)
+  def test_print_3(self):
+    w = StringIO.StringIO()
+    res = [1, 2, 3]
+    xml_print(w, res)
+    self.assert_(w.getvalue() == "3\n1\n2\n3\n")
 
-    # -----------
-    # parseTree 
-    # -----------
+  # -----------
+  # parseTree 
+  # -----------
 
-    def test_parseTree_1(self):
-        self.assert_(0)
+  def test_parseTree_1(self):
+    self.assert_(0)
 
-    def test_parseTree_2(self):
-        self.assert_(0)
+  def test_parseTree_2(self):
+    self.assert_(0)
 
-    def test_parseTree_3(self):
-        self.assert_(0)
+  def test_parseTree_3(self):
+    self.assert_(0)
 
-    # --------
-    # dfs_new 
-    # --------
+  # --------
+  # dfs_new 
+  # --------
 
-    def test_dfs_new_1(self):
-        self.assert_(0)
+  def test_dfs_new_1(self):
+    self.assert_(0)
 
-    def test_dfs_new_2(self):
-        self.assert_(0)
+  def test_dfs_new_2(self):
+    self.assert_(0)
 
-    def test_dfs_new_2(self):
-        self.assert_(0)
+  def test_dfs_new_3(self):
+    self.assert_(0)
 
-    # -----
-    # solve
-    # -----
+  # -----
+  # solve
+  # -----
 
-    def test_solve_1(self):
-        self.assert_(0)
+  def test_solve_1(self):
+    self.assert_(0)
 
-    def test_solve_2(self):
-        self.assert_(0)
+  def test_solve_2(self):
+    self.assert_(0)
 
-    def test_solve_3(self):
-        self.assert_(0)
+  def test_solve_3(self):
+    self.assert_(0)
 # ----
 # main
 # ----
